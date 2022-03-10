6915450831b923c8dbc529a6ef7625cb4133339f
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-10 03:00:57 -0800

    created my first C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * This progam is designed to print without printf
+ * main - This print Programming is like a multilingual puzz;e
+ * Return:0 if cide runs properly and otherwise
  */
-
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	
-	return(0);
+	return (0);
 }
