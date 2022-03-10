f48295d1d5e1483afe8bad3b131e6d336dc9601c
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-10 03:46:23 -0800

    initial commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the size of variou data type
+ * Return:0 when code runs perfectly
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+	printf("Size of a char: %d bytes(s)\n",sizeof(c));
+	printf("Size of a char: %d bytes(s)\n",sizeof(i));
+	printf("Size of a char: %d bytes(s)\n",sizeof(li));
+	printf("Size of a char: %d bytes(s)\n",sizeof(lli));
+	printf("Size of a char: %d bytes(s)\n",sizeof(f));
+	return (0);
