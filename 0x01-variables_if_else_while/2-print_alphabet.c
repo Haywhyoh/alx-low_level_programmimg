e705df10bdb352da2c89eecd8bf66f4867cb6541
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-11 09:24:37 -0800

    Initial commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints alhabets 
+ * in lower case
+ *
+ * Returns: 0 if code runs succesfuly
+ */
+int main(void)
+{
+	char alphabet [27];
+
+	alphabet = "abcdefghijklmnopqrstuvwxyz";
+	putchar(alphabet);
+	return (0);
+}
