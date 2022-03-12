010153cb0b922c40be26c55521b0a1fed2528c0c
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-11 17:31:42 -0800

    inital commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,9 +2,9 @@
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
  * followed by a new line, using putchar
- * 
+ *
  * Return: Always 0 (Success)
- *    
+ *
  */
 int main(void)
 {
