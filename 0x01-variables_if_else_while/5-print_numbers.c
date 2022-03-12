73f6f503db99a0f43b5917396a815e194abda8fc
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-11 17:10:17 -0800

    initial commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all code from 0-9
+ *
+ * Return: 0 if code is succesful
+ */
+int main(void)
+{
+	int num;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
