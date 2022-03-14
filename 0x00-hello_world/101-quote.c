9a88e33f3b8e0513d9a76e5213df7dbc5100ec88
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-14 03:12:14 -0700

    initail commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this function prints
+ * and that piece of art is useful"-
+ * Dora Korpar, 2015-10-09
+ *
+ * Returns: 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora");
+	puts(" Korpar, 2015-10-19");
+	return (1);
+}
