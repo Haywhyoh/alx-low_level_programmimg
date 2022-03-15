28a7d84c87a6d2e545130743c4feb93aecb64fa5
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-15 02:19:33 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints alphabet in 
+ * lower case
+ *
+ * Return: 1 when c is lower case
+ * then 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
