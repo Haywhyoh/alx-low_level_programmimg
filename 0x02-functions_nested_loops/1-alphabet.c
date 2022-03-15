54d4902ea2ab78278284e5a8322e188b9fc0c3ee
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-15 01:32:37 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints alphabet in
+ * lower case
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+}
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+	return (0);
+}
