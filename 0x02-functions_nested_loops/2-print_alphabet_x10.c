53db8cb692c37e9aab65ad5254774e35cc356153
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-15 01:44:15 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - print 10x the alphabet
+ *
+ * Return: 0 when succesful
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
