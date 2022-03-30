d7e0c5f8732adabe93f14f6fe6ee7522597b1961
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-30 05:52:42 -0700

    changed the function name

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,12 @@
  *
  * Return: len the strng length
  */
-int strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 
 	if (*s != '\0')
 	{
 		return (0);
 	}
-	return (strlen_recursion(s + 1) + 1);
+	return (_strlen_recursion(s + 1) + 1);
 }
