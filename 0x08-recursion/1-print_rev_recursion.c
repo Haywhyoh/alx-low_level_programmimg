fcb3b514ae0ced832479da8d4719b792aee702d2
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-30 05:27:15 -0700

    initial commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_rev_recursion - prints a
+ * _print_rev_recursion - prints a
  * string in reverse
  * @s: the string to be printed
  *
@@ -11,8 +11,8 @@ void _print_rev_recursion(char *s)
 
 	if (*s == '\0')
 	{
-		_putchar(*s);
 		_print_rev_recursion(s - 1);
+		-putchar(*s);
 	}
 
 }
