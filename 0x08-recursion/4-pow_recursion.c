c91b9f96b07eb645109e70c2197d84f6317549a1
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-30 10:20:55 -0700

    created a factorial.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns the power of x by y
+ * @x: the base
+ * @y: the power
+ *
+ * Return: the final answer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 1)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _power_recursion(x, (y - 1));
+}
