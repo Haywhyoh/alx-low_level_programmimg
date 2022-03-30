ea53366f23d1a6e3c81882dc630229bdb787ea27
Haywhyoh <adefeyisayo998@gmail.com>
2022-03-30 04:25:43 -0700

    initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _puts_recursion prints a string
+ * followed by a new line
+ * @*s: is the string printed
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n);
+	}
+	putchar(*s);
+	_puts_recursion(s++);
+}
