78d9c723e21ed6c63d5ee8998968cbc6f18e3e06
Haywhyoh <adefeyisayo998@gmail.com>
2022-04-01 10:21:17 -0700

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * whatsmayname.c -  prints it's name followed by a new line
+ *
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv)
+{   
+	printf("%s\n"argv[0]);
+}
