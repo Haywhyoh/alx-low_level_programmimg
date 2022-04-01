38c9c4a51a00e82f643d57337f6682b918a5b550
Haywhyoh <adefeyisayo998@gmail.com>
2022-04-01 11:36:46 -0700

    added a descripton

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * main - prints the number of argument
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	printf("%i\n", argc - 1);
