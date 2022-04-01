3c13314a6f429098f40188e2ec72006a27348c83
Adedayo Samuel Ayomide <75112647+Haywhyoh@users.noreply.github.com>
2022-04-01 19:09:59 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * whatsmayname.c -  prints it's name followed by a new line
+ * main - prints the name of the functon
+ * @argv: argument vector
+ * @argc: argument count
  *
  * Return: 0
  */
