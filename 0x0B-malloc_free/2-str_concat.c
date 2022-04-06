d207ed2bcd4a0498163d3bb9418f95af4f172f6b
Haywhyoh <adefeyisayo998@gmail.com>
2022-04-05 19:20:33 -0700

    initial commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdlib>
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string to concat
+ * @s2: second string to concat
+ *
+ * Return: pointer to the new string concated(success), Null for error
+ */
+
