23c504071998669ad3d4a301875f59c8f9e04d53
Haywhyoh <adefeyisayo998@gmail.com>
2022-04-05 18:25:21 -0700

    initial commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strdup - duplicates the string given
+ * @str: string to duplicate
+ *
+ * Return: NULL for error, 0 for success
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int len, i;
+
+	if (*str == NULL)
+	{
+		return (NULL);
+	}
+
+	while(str[len])
+		len++;
+	
+	dup = malloc((len + 1) * sizeof(char));
+
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+	
+	while ((str[i] == dup[i]) != '\0')
+		i++;
+
+	return (dup);
+}
