bc001a0beeecb1959f50a1444bf78ab81c493a3e
Adedayo Samuel Ayomide <75112647+Haywhyoh@users.noreply.github.com>
2022-04-12 07:59:09 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,20 @@
-#include "dog.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
 
 /**
- * print_dog - prints dog data
- * @d: dog struct
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
  */
-
 void print_dog(struct dog *d)
 {
-if (d != NULL)
-{
-	printf("Name: %s\nAge: %f\nOwner: %s\n",
-	(*d).name != NULL ? d->name : "(nil)",
-	(*d).age,
-	(*d).owner != NULL ? d->owner : "(nil)");
-}
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
