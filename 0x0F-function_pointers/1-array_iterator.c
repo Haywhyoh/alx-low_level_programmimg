6c2c0ab055f60848604ba8ce9d482886efc26641
Haywhyoh <adefeyisayo998@gmail.com>
2022-04-13 16:18:49 -0700

    created aray_iteratorc

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - execute a function on every 
+ * elememt of an array
+ * @array: the given array 
+ * @size: the size of the array
+ * @ action: the function that is executed on array elements
+ *
+ */
+void array_iterator(int *array, size_t size t, void (*action)(int))
+{
+	int i;
+
+	if (!array || action)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
