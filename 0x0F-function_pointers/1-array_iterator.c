008ea138508f02292bd07d4c225cc7be0cfda15a
Haywhyoh <adefeyisayo998@gmail.com>
2022-04-13 16:24:39 -0700

    fixed betty codes

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,10 +5,10 @@
  * elememt of an array
  * @array: the given array
  * @size: the size of the array
- * @ action: the function that is executed on array elements
+ * @action: the function that is executed on array elements
  *
  */
-void array_iterator(int *array, size_t size t, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
