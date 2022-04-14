7f6de806b0dd2fdf4a09f941a7333b32affc77e9
Adedayo Samuel Ayomide <75112647+Haywhyoh@users.noreply.github.com>
2022-04-15 00:57:53 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,8 +2,11 @@
 #define VAR_FUNC_H
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
-#endif
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
+#endif
