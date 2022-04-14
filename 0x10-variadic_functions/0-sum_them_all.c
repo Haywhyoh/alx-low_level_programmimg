54672bcd9dfa11cdd74f11e24fbffedbcf6e5da4
Haywhyoh <adefeyisayo998@gmail.com>
2022-04-14 10:30:28 -0700

    created a variadiv_function.h

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "variadic_functions.h"
 /**
  * sum_them_all - return sum on all parameters
@@ -12,11 +10,11 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
-	sum 0;
+	int sum = 0;
 
 	if (n == 0)
 		return (0);
-	va_start(n, ap);
+	va_start(ap, n);
 	for (i =  0; i < n; i++)
 		sum += va_arg(ap, int);
 	va_end(ap);
