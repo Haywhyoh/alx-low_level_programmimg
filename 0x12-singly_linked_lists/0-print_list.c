9d7ec1ad92aed23406308b8069b32d4564e4268b
Haywhyoh <adefeyisayo998@gmail.com>
2022-04-21 06:15:00 -0700

    fixed the h = h->str bug

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,15 @@
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
-	
+
 	while (h)
 	{
-	if (h->str == NULL)
-		printf("[0] nil \n");
-	else
-		printf("[%u] %s\n", h->len, h->str);
-	h->next;
-	s++;
+		if (h->str == NULL)
+			printf("[0] nil \n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
 	}
 
 	return (s);
