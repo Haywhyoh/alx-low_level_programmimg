ce98076f44a2003a860422aa201a710a7a59566c
Haywhyoh <adefeyisayo998@gmail.com>
2022-04-26 00:06:27 -0700

    added add_mode.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add new node at the beginnig of a list
+ * @head: the first node
+ * @str: strin to be added
+ *
+ * Return - the address of the new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *dup = str;
+	
+
+}
