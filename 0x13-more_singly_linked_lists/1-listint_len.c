3b7de1e62a0f90ce38ff78ceb0e58ff79bf9cfd6
Haywhyoh <adefeyisayo998@gmail.com>
2022-04-26 01:12:11 -0700

    created listint.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "list.h"
+/**
+ * listint_len - return the number of elements in a
+ * linked list.
+ * @h: the first node of the linked list
+ *
+ * Return: the number of elemens
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
