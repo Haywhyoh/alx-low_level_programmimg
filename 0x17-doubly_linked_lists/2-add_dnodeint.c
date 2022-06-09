a1cf0c45743538bbb01191d65758c2e87dd7ec2e
Haywhyoh <adefeyisayo998@gmail.com>
2022-06-09 04:49:22 -0700

    fixed task 3

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * dlistint_t *add_dnodeint - add a nes node to the begining
+ * of a linked list
+ * @head: pointer to the head node
+ * @n: the integer value to be added to the linked list
+ *
+ * Return: address of the new element or NULL if failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+	{
+		return (NULL);
+	}
+
+	if (*head)
+	{
+		new_node->n = n;
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		(*head)->prev = new_node;
+	}
+
+	new_node->next = *head;
+	new_node->prev = NULL;
+	new_node->n = n;
+
+	*head = new_node;
+
+	return (new_node);
+}
