0ca7e878f2edc076936964544561410d22900c0d
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-06-30 10:44:26 +0100

    completed task 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,28 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table[size];
+    hash_table_t *hash_table;
+    unsigned long int i;
+
+    if (size < 0)
+        return (NULL);
+
+    hash_table = malloc(sizeof(hash_table_t));
+
+    if (!hash_table)
+        return (NULL);
+
+    hash_table->size = size;
+    hash_table->array = malloc(size * sizeof(hash_node_t));
+
+    if (!(hash_table->array))
+        return (NULL);
+    
+    for (i = 0; i <= size; i++)
+    {
+        hash_table->array[i] = NULL;
+
+    }
 
     return (hash_table);
 }
