fa247a37266eb7cdbd26a720762abc98453a35c5
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-06-30 09:56:56 +0100

    did task 1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * hash_table_t- *hash_table_create: function to crate hash table
+ * @size: size of the hash table
+ * 
+ * Return:  a pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *hash_table[size];
+
+    return (hash_table);
+}
