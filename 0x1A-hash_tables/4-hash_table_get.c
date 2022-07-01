40a07c967716aeca7dc2cc08613e4a26d3c57d3b
Haywhyoh <adefeyisayo998@gmail.com>
2022-07-01 09:24:05 -0700

    did task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function that retrieves a value associated with a key.
+ * @ht: the hash table
+ * @key: the key whose value is to be retrieved
+ *
+ * Return: value associated with element or NULL if key not found
+ */
+#include "hash_tables.h"
+/**
+ * hash_table_get - get value from key
+ * @ht: hashtable
+ * @key: key to find value for
+ * Return: value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index;
+	hash_node_t *check;
+
+	if (ht == NULL || strlen(key) == 0)
+		return (NULL);
+
+	index = key_index((const unsigned char *) key, ht->size);
+	check = ht->array[index];
+	if (check == NULL)
+		return (NULL);
+
+	while (check != NULL)
+	{
+		if (strcmp(key, check->key) == 0)
+		{
+			return (check->value);
+		}
+		check = check->next;
+	}
+	return (NULL);
+}
